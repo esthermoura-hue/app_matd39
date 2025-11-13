ce6151b31b038cafcd41f10f9732ce71fb80986b
esthermoura-hue <esther.moura@ufba.br>
2025-11-13 10:31:20 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,2 +1,5 @@
 import streamlit as st
 st.title('Meu primeiro app ❤')
+st.header('Vamos fazer algo com interatividade')
+n= st.number_input('Entre com seu numero')
+st.write(f'o numero que vc escolheu ao quadrado é(n**))'
